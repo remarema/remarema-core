9396c45e01ffeb237440f5a2167f93f9342fbd9d
Regina1014 <regiwerlberger@yahoo.de>
2015-04-26 16:26:51 +0200

    scron

--- remarema-core/src/remarema/core/Main.java
+++ remarema-core/src/remarema/core/Main.java
@@ -1,5 +1,15 @@
 package remarema.core;
 
+import java.io.File;
+
 public class Main {
+	FileRepository re=new FileRepository();
+	private String directory;
+public File sycronFile(String directory1, String directory2){
+re.listFiles(directory);
+	return null;
+	
+}
+	
 
 }
